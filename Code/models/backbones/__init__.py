a2a02383cc7be1d5a5cf905cc14d7569fa998a64
guanjinquan <1666320330@qq.com>
2024-08-25 21:25:17 +0800

    fixed backbones.__init__.py bug

--- Code/models/backbones/__init__.py
+++ Code/models/backbones/__init__.py
@@ -1,5 +1,6 @@
 from models.backbones.resnet_imagenet import ResNetImagenet as resnet_imagenet
-from models.backbones.resnet50_pathology import ResNetPathology as resnet50_pathology
-from models.backbones.swin_imagenet import SwinImageNet as swin_imagenet
 from models.backbones.vit_small_p16_imagenet import VitImagenet as vit_small_p16_imagenet
-from models.backbones.vit_small_p16_pathology import VitPathology as vit_small_p16_pathology
+
+# task-3 need to construct these new models
+# from models.backbones.resnet50_pathology import ResNetPathology as resnet50_pathology
+# from models.backbones.vit_small_p16_pathology import VitPathology as vit_small_p16_pathology
